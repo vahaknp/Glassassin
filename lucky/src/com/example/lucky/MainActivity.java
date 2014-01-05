65de6c77d7989b15e4e98d6056a9942b7ad28390
SourenP <souren@papazian.info>
2014-01-05 08:03:36 -0500

    Added lucky
    Base app that runs on glass with voice command
    To change voice command change the variable "voice_command" in
    lucky/res/values/strings.xml

---
+++ lucky/src/com/example/lucky/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lucky;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.glass.app.Card;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Card card = new Card(this);
+        card.setText("Hello Tumo!");
+        card.setFootnote("glass workshop");
+
+        setContentView(card.toView());
+	}
+
+}
