daee70c2e79411002863664b26abd35fe8ee21f0
Tigran Tokmajyan <toktikweb@gmail.com>
2014-01-06 15:47:11 +0400

    fix service

--- toktik-sample/src/com/tumo/reminder/ReminderService.java
+++ toktik-sample/src/com/tumo/reminder/ReminderService.java
@@ -1,6 +1,11 @@
 package com.tumo.reminder;
 
+import com.google.android.glass.timeline.LiveCard;
+import com.google.android.glass.timeline.TimelineManager;
+
+import android.R;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
@@ -8,16 +13,35 @@ import android.widget.Toast;
 
 public class ReminderService extends Service {
 
+	private static final String LIVE_CARD_TAG = "reminder_tag";
+	private LiveCard mLiveCard;
+	
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
 	
 	@Override
+	public void onCreate() {
+		Toast.makeText(this, "ReminderService onCreate", Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		Log.i("ReminderService", "Received start id " + startId + ":" + intent);
-		Toast.makeText(this, "ReminderService started", 1).show();
+		Toast.makeText(this, "ReminderService onStartCommand", Toast.LENGTH_SHORT).show();
 		return START_STICKY;
 	}
 	
+	private void publishCard(Context context) {
+		if(mLiveCard == null) {
+			TimelineManager tm = TimelineManager.from(this);
+			mLiveCard = tm.createLiveCard(LIVE_CARD_TAG);
+			
+//			mLiveCard.setViews(new RemoteViews(context.getPackageName(),
+//					R.layout.card_text))
+		} else {
+			// card is already published
+			return;
+		}
+	}
 }
