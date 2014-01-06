ea9bb68683cadf7a76df732cfd0443bdb17ac1a2
Tigran Tokmajyan <toktikweb@gmail.com>
2014-01-06 14:00:31 +0400

    Finished sample app

--- toktik-sample/src/com/example/toktiksample/MainActivity.java
+++ toktik-sample/src/com/example/toktiksample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.toktiksample;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 
 import com.google.android.glass.app.Card;
 
@@ -11,9 +14,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ArrayList<String> voiceResults = getIntent().getExtras()
+        		.getStringArrayList(RecognizerIntent.EXTRA_RESULTS);
+        
         Card card = new Card(this);
         card.setText("This is sample app! That works.");
-        card.setFootnote("at Tumo...");
+        card.setFootnote(voiceResults.get(0) + " is great place!");
 
         setContentView(card.toView());
 	}
